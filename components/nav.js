a24a65a53befd4aee65f66d0017694fad0224732
maxst <lunabright817@gmail.com>
2021-11-03 09:47:45 +0900

    re-init

--- components/nav.js
+++ components/nav.js
@@ -1,26 +1,3 @@
-import Link from "next/link";
-
-import styles from "../styles/Nav.module.css";
-
 export default function Nav() {
-  const brands = [
-    "Grishko",
-    "Russian Pointe",
-    "Bloch",
-    "Capezio",
-    "Gaynor minden",
-  ];
-  return (
-    <nav className={styles.navBox}>
-      <ul className={styles.navItems}>
-        {brands.map((brand) => {
-          return (
-            <li key={brand} className={styles.navItem}>
-              <Link href={`/${brand}`}>{brand}</Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
-  );
+  return <div>nav</div>;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,13 @@
-import { useEffect, useState } from "react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-
-import Main from "../components/main";
-import Layout from "../components/layout";
+import Nav from "../components/Nav";
 
 export default function Home() {
-  const [products, setProducts] = useState(null);
-
-  useEffect(() => {
-    fetch("/data/data.json")
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        setProducts(res.data);
-      });
-  }, []);
-
   return (
-    <Layout>
+    <>
       <Head>
-        <title>the pointe</title>
+        <title>shop</title>
       </Head>
-      <Main products={products} />
-    </Layout>
+      <Nav />
+    </>
   );
 }
